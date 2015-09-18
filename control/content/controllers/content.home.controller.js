dd7a73af6d68ccc9c39272b7357f8c235f731aa3
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-09-18 13:38:38 +0530

    Added angular app structure for content section

---
+++ control/content/controllers/content.home.controller.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+(function (angular) {
+  angular
+    .module('eventsFeedPluginContent')
+    .controller('ContentHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
