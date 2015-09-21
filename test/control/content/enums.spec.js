3303fd8f2350b4168a5d1d93878a4b3d95a6e7d5
tabish.khan <tabish.khan@tothenew.com>
2015-09-21 17:21:26 +0530

    Added test case of enum.js file

---
+++ test/control/content/enums.spec.js
@@ -0,0 +1,33 @@
+describe('Unit : eventsFeedPlugin content Enums', function () {
+  var TAG_NAMES, STATUS_CODE, ADDRESS_TYPE;
+  beforeEach(module('eventsFeedPluginContent'));
+
+  beforeEach(inject(function (_TAG_NAMES_, _STATUS_CODE_, _ADDRESS_TYPE_) {
+    TAG_NAMES = _TAG_NAMES_;
+    STATUS_CODE = _STATUS_CODE_;
+    ADDRESS_TYPE = _ADDRESS_TYPE_;
+  }));
+
+  describe('Enum : TAG_NAMES', function () {
+    it('TAG_NAMES should exist and be an object', function () {
+      expect(typeof TAG_NAMES).toEqual('object');
+    });
+  });
+
+  describe('Enum : STATUS_CODE', function () {
+    it('STATUS_CODE should exist and be an object', function () {
+      expect(typeof STATUS_CODE).toEqual('object');
+    });
+    it('STATUS_CODE.INSERTED should exist and equals to "inserted"', function () {
+      expect(STATUS_CODE.INSERTED).toEqual('inserted');
+    });
+    it('STATUS_CODE.UPDATED should exist and equals to "updated"', function () {
+      expect(STATUS_CODE.UPDATED).toEqual('updated');
+    });
+    it('STATUS_CODE.NOT_FOUND should exist and equals to "NOTFOUND"', function () {
+      expect(STATUS_CODE.NOT_FOUND).toEqual('NOTFOUND');
+    });
+  });
+
+
+});
