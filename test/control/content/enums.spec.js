ebc01bc375d0ef0b6700a8c238e9310be42d1a3b
tabish.khan <tabish.khan@tothenew.com>
2015-09-21 20:29:09 +0530

    updated the test cases of enums.js

--- test/control/content/enums.spec.js
+++ test/control/content/enums.spec.js
@@ -1,11 +1,11 @@
 describe('Unit : eventsFeedPlugin content Enums', function () {
-  var TAG_NAMES, STATUS_CODE, ADDRESS_TYPE;
+  var TAG_NAMES, STATUS_CODE, STATUS_MESSAGES;
   beforeEach(module('eventsFeedPluginContent'));
 
-  beforeEach(inject(function (_TAG_NAMES_, _STATUS_CODE_, _ADDRESS_TYPE_) {
+  beforeEach(inject(function (_TAG_NAMES_, _STATUS_CODE_, _STATUS_MESSAGES_) {
     TAG_NAMES = _TAG_NAMES_;
     STATUS_CODE = _STATUS_CODE_;
-    ADDRESS_TYPE = _ADDRESS_TYPE_;
+    STATUS_MESSAGES = _STATUS_MESSAGES_;
   }));
 
   describe('Enum : TAG_NAMES', function () {
