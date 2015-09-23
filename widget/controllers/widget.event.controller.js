e99d572fd307f711a9160ac67df88e5e7a38977d
tabish.khan <tabish.khan@tothenew.com>
2015-09-23 21:15:29 +0530

    Implemented the widget sync with design part.

--- widget/controllers/widget.event.controller.js
+++ widget/controllers/widget.event.controller.js
@@ -2,13 +2,13 @@
 
 (function (angular) {
   angular.module('eventsFeedPluginWidget')
-    .controller('WidgetEventCtrl', ['$scope', 'DataStore', 'TAG_NAMES', 'Location', '$routeParams', 'CalenderFeedApi', 'LAYOUTS',
-      function ($scope, DataStore, TAG_NAMES, Location, $routeParams, CalenderFeedApi, LAYOUTS) {
+    .controller('WidgetEventCtrl', ['$scope', 'DataStore', 'TAG_NAMES', 'Location', '$routeParams', 'CalenderFeedApi', 'LAYOUTS','Buildfire',
+      function ($scope, DataStore, TAG_NAMES, Location, $routeParams, CalenderFeedApi, LAYOUTS,Buildfire) {
 
         var WidgetEvent = this;
         WidgetEvent.data = null;
         WidgetEvent.event = null;
-
+          var currentListLayout = null;
         var getEventDetails = function (url) {
           var success = function (result) {
               console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", result);
@@ -21,10 +21,71 @@
             CalenderFeedApi.getSingleEventDetails(url, $routeParams.eventIndex).then(success, error);
         };
 
-        /*
-         * Fetch user's data from datastore
-         */
+          /*declare the device width heights*/
+          WidgetEvent.deviceHeight = window.innerHeight;
+          WidgetEvent.deviceWidth = window.innerWidth;
 
+          /*initialize the device width heights*/
+          function initDeviceSize(callback) {
+              WidgetEvent.deviceHeight = window.innerHeight;
+              WidgetEvent.deviceWidth = window.innerWidth;
+              if (callback) {
+                  if (WidgetEvent.deviceWidth == 0 || WidgetEvent.deviceHeight == 0) {
+                      setTimeout(function () {
+                          initDeviceSize(callback);
+                      }, 500);
+                  } else {
+                      callback();
+                      if (!$scope.$$phase && !$scope.$root.$$phase) {
+                          $scope.$apply();
+                      }
+                  }
+              }
+          }
+
+          /*crop image on the basis of width heights*/
+          WidgetEvent.cropImage = function (url, settings) {
+              var options = {};
+              if (!url) {
+                  return "";
+              }
+              else {
+                  if (settings.height) {
+                      options.height = settings.height;
+                  }
+                  if (settings.width) {
+                      options.width = settings.width;
+                  }
+                  return Buildfire.imageLib.cropImage(url, options);
+              }
+          };
+            /*update data on change event*/
+          var onUpdateCallback = function (event) {
+              setTimeout(function () {
+                  $scope.imagesUpdated = false;
+                  $scope.$digest();
+                  if (event && event.tag === TAG_NAMES.EVENTS_FEED_INFO) {
+                      WidgetEvent.data = event.data;
+                      if (!WidgetEvent.data.design)
+                          WidgetEvent.data.design = {};
+                      if (!WidgetEvent.data.content)
+                          WidgetEvent.data.content = {};
+                  }
+                  if (!WidgetEvent.data.design.itemDetailsLayout) {
+                      WidgetEvent.data.design.itemDetailsLayout = LAYOUTS.itemDetailsLayout[0].name;
+                  }
+
+                  currentListLayout = WidgetEvent.data.design.itemDetailsLayout;
+                  console.log("hhhhh",currentListLayout)
+                  $scope.imagesUpdated = !!event.data.content;
+                  $scope.$digest();
+              }, 0);
+          };
+          DataStore.onUpdate().then(null, null, onUpdateCallback);
+
+          /*
+           * Fetch user's data from datastore
+           */
         var init = function () {
           var success = function (result) {
               WidgetEvent.data = result.data;
