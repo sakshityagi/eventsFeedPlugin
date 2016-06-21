3053f14c22e111dda90778abf381b8f2677cd2a9
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-06-21 14:20:50 +0530

    Correct bg image logic, added directive for it.

--- widget/app.js
+++ widget/app.js
@@ -114,6 +114,38 @@
         }
       };
     }])
+    .directive('backImg', ["$rootScope", function ($rootScope) {
+      return function (scope, element, attrs) {
+        attrs.$observe('backImg', function (value) {
+          var img = '';
+          if (value) {
+            buildfire.imageLib.local.cropImage(value, {
+              width: $rootScope.deviceWidth,
+              height: $rootScope.deviceHeight
+            }, function (err, imgUrl) {
+              if (imgUrl) {
+                img = imgUrl;
+                element.attr("style", 'background:url(' + img + ') !important');
+              } else {
+                img = '';
+                element.attr("style", 'background-color:white');
+              }
+              element.css({
+                'background-size': 'cover'
+              });
+            });
+            // img = $filter("cropImage")(value, $rootScope.deviceWidth, $rootScope.deviceHeight, true);
+          }
+          else {
+            img = "";
+            element.attr("style", 'background-color:white');
+            element.css({
+              'background-size': 'cover'
+            });
+          }
+        });
+      };
+    }])
     .config(function ($provide) {    //This directive is used to add watch in the calendar widget
       $provide.decorator('datepickerDirective', ['$delegate', '$rootScope', function ($delegate, $rootScope) {
         var directive = $delegate[0];
